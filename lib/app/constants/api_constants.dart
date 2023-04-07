d0c46ff06e68cd9377228ebab8cc857a99e48ef7
Ablay Akhmetov <ablay.a@aloteq.com>
2023-04-07 16:09:42 +0600

    added Dio initialization

---
+++ lib/app/constants/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static const String baseApiUrl = 'https://catfact.ninja';
+}
--- lib/app/di/injector.dart
+++ lib/app/di/injector.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_cat_facts/app/app_router/app_router.dart';
+import 'package:flutter_cat_facts/app/constants/api_constants.dart';
+import 'package:flutter_cat_facts/data/dio/dio_factory.dart';
 import 'package:flutter_cat_facts/presentation/context/bloc/context_activity_bloc.dart';
 import 'package:flutter_cat_facts/presentation/fact_screen/bloc/fact_bloc.dart';
 import 'package:flutter_cat_facts/presentation/facts_history_screen/bloc/facts_history_bloc.dart';
@@ -8,12 +11,15 @@ GetIt getIt = GetIt.instance;
 
 class Injector {
   static void initDependencies() {
-    ///Init blocs
+    /// Init blocs
     getIt.registerLazySingleton<ContextActivityBloc>(() => ContextActivityBloc());
     getIt.registerLazySingleton<FactBloc>(() => FactBloc());
     getIt.registerLazySingleton<FactsHistoryBloc>(() => FactsHistoryBloc());
 
-    ///Init appRoute
+    /// Init appRoute
     getIt.registerLazySingleton<AppRouter>(() => AppRouter());
+
+    /// Init Dio
+    getIt.registerLazySingleton<Dio>(() => DioFactory().create(ApiConstants.baseApiUrl));
   }
 }
